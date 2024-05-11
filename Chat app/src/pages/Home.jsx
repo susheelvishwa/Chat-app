8cc60461ec883183e907371928ca817d472c04c2
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-05-11 13:17:15 +0530

    Chat app

--- Chat app/src/pages/Home.jsx
+++ Chat app/src/pages/Home.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
 
 
 const Home = () => {
     return (
-        <div className="home">
-            <div className="container">
-                <Sidebar />
-                <Chat />
+      <div className="home">
+        <div className="container">
+          <Sidebar />
+          <Chat />
         </div>
-    </div>
+      </div>
     );  
 }
 
